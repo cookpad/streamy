c27e8f7a5a543a116250f3c7b77f9dddaa79bbec
Jens Balvig <jens@balvig.com>
2017-04-18 19:57:53 +0900

    Gzip as last step

--- lib/streamy/event_stores/redshift/importer.rb
+++ lib/streamy/event_stores/redshift/importer.rb
@@ -8,13 +8,10 @@ module Streamy
 
         def import
           pipe_messages_to_file
+          gzip_file
           upload_file
           yield
           remove_file
-        end
-
-        def upload_file
-          file_store.upload_file(file_path)
         end
 
         private
@@ -25,8 +22,24 @@ module Streamy
             Streamy.message_bus = FileMessageBus.new(file_path)
           end
 
+          def gzip_file
+            `gzip #{file_path}`
+          end
+
+          def upload_file
+            file_store.upload_file(gzip_file_path)
+          end
+
+          def remove_file
+            FileUtils.rm(gzip_file_path)
+          end
+
           def file_path
             Pathname.new(Dir.home).join("domain_events_export.json.gz")
+          end
+
+          def gzip_file_path
+            file_path + ".gz"
           end
 
           def file_store
@@ -38,10 +51,6 @@ module Streamy
 
           def remote_path
             config[:folder] + "/" + File.basename(file_path)
-          end
-
-          def remove_file
-            FileUtils.rm(file_path)
           end
       end
     end
