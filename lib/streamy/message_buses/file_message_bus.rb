2dbb80bb881ad223626634f5c4940f5bcc9f3c98
Jens Balvig <jens@balvig.com>
2017-04-20 13:26:31 +0900

    update from master

--- lib/streamy/message_buses/file_message_bus.rb
+++ lib/streamy/message_buses/file_message_bus.rb
@@ -1,27 +1,25 @@
-module Streamy
-  module MessageBuses
-    class FileMessageBus
-      def initialize(file_path)
-        @file_path = file_path
-      end
-
-      def deliver(data)
-        write_to_file convert_to_redshift_json(data)
-      end
-
-      private
-
-        attr_reader :file_path, :deliveries
-
-        def write_to_file(data)
-          Zlib::GzipWriter.open(file_path) do |gz|
-            gz.write(data + "\n")
-          end
-        end
-
-        def convert_to_redshift_json(data)
-          data.merge(timestamp: Time.current).to_json
-        end
+module EventStream
+  class FileMessageBus
+    def initialize(file_path)
+      @file_path = file_path
     end
+
+    def deliver(data)
+      write_to_file convert_to_redshift_json(data)
+    end
+
+    private
+
+      attr_reader :file_path
+
+      def write_to_file(data)
+        File.open(file_path, "a") do |file|
+          file.write(data)
+        end
+      end
+
+      def convert_to_redshift_json(data)
+        data.merge(timestamp: Time.current).to_json
+      end
   end
 end
