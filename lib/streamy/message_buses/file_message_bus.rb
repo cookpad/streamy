f15a521bfa26f7dc8a426568f9086375bc61dc14
Jens Balvig <jens@balvig.com>
2017-04-20 14:33:44 +0900

    fix namespace

--- lib/streamy/message_buses/file_message_bus.rb
+++ lib/streamy/message_buses/file_message_bus.rb
@@ -1,25 +1,27 @@
-module EventStream
-  class FileMessageBus
-    def initialize(file_path)
-      @file_path = file_path
-    end
+module Streamy
+  module MessageBuses
+    class FileMessageBus
+      def initialize(file_path)
+        @file_path = file_path
+      end
 
-    def deliver(data)
-      write_to_file convert_to_redshift_json(data)
-    end
+      def deliver(data)
+        write_to_file convert_to_redshift_json(data)
+      end
 
-    private
+      private
 
-      attr_reader :file_path
+        attr_reader :file_path
 
-      def write_to_file(data)
-        File.open(file_path, "a") do |file|
-          file.write(data)
+        def write_to_file(data)
+          File.open(file_path, "a") do |file|
+            file.write(data)
+          end
         end
-      end
 
-      def convert_to_redshift_json(data)
-        data.merge(timestamp: Time.current).to_json
-      end
+        def convert_to_redshift_json(data)
+          data.merge(timestamp: Time.current).to_json
+        end
+    end
   end
 end
