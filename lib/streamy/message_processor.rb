ebf4ad94cad38936ae32c977d52aafdb9f8a2cfc
Jens Balvig <jens@balvig.com>
2017-06-16 15:24:05 +0900

    use instantiation to make it easier to use

--- lib/streamy/message_processor.rb
+++ lib/streamy/message_processor.rb
@@ -1,46 +1,48 @@
 module Streamy
   class MessageProcessor
-    def self.process(message)
-      new(message).process
+    def process(message)
+      ProcessEvent.new(message).run
     end
 
-    def initialize(message)
-      @message = message
+    class ProcessEvent
+      def initialize(message)
+        @message = message
+      end
+
+      def run
+        event_handler.new(attributes).process
+      end
+
+      private
+
+        attr_reader :message
+
+        def event_handler
+          handler_class_name.safe_constantize || raise(handler_not_found_error)
+        end
+
+        def handler_class_name
+          "EventHandlers::#{message_type.camelize}"
+        end
+
+        def message_type
+          message.type || raise(TypeNotFoundError)
+        end
+
+        def handler_not_found_error
+          EventHandlerNotFoundError.new(handler_class_name)
+        end
+
+        def attributes
+          {
+            body: body,
+            event_time: message.event_time
+          }
+        end
+
+        def body
+          (message.body || {}).with_indifferent_access
+        end
     end
-
-    def process
-      handler.new(attributes).process
-    end
-
-    private
-
-      attr_reader :message
-
-      def handler
-        handler_class_name.safe_constantize || raise(handler_not_found_error)
-      end
-
-      def handler_class_name
-        "EventHandlers::#{message_type.camelize}"
-      end
-
-      def message_type
-        message.type || raise(TypeNotFoundError)
-      end
-
-      def handler_not_found_error
-        EventHandlerNotFoundError.new(handler_class_name)
-      end
-
-      def attributes
-        {
-          body: body,
-          event_time: message.event_time
-        }
-      end
-
-      def body
-        (message.body || {}).with_indifferent_access
-      end
   end
 end
