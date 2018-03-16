9131c8bf2f037885d8fb65c35abde48409e0313a
David Stosik <david.stosik+git-noreply@gmail.com>
2018-03-16 15:40:13 +0000

    Use same logger for Hutch and Streamy

--- lib/streamy/consumer.rb
+++ lib/streamy/consumer.rb
@@ -4,6 +4,7 @@ module Streamy
   module Consumer
     def self.included(base)
       base.include Hutch::Consumer
+      Hutch::Logging.logger = Streamy.logger
       base.consume "#{Streamy::DEFAULT_TOPIC_PREFIX}.#"
 
       configure_hutch
