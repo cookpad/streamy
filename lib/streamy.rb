654980655b24008e126201f16c288f728f525d98
Jens Balvig <jens@balvig.com>
2018-03-18 10:34:32 +0000

    prepare test

--- lib/streamy.rb
+++ lib/streamy.rb
@@ -4,6 +4,7 @@ module Streamy
   # External
   # TODO: Move into classes that use them
   require "active_support"
+  require "active_support/core_ext/string"
 
   require "streamy/version"
   require "streamy/consumer"
--- test/consumer_test.rb
+++ test/consumer_test.rb
@@ -1,16 +1,37 @@
 require "test_helper"
 
-module Streamy
-  class ConsumerTest < Minitest::Test
-    class DummyConsumer
-      include Consumer
+class DummyConsumer
+  include Streamy::Consumer
+end
+
+module EventHandlers
+  class DummyEvent < Streamy::EventHandler
+    cattr_accessor :times_called do
+      0
     end
 
+    def process
+      self.times_called += 1
+    end
+  end
+end
+
+module Streamy
+  class ConsumerTest < Minitest::Test
     def test_processing_empty_message
       message = {}
       assert_raises(TypeNotFoundError) do
         DummyConsumer.new.process(message)
       end
+    end
+
+    def test_ignoring_duplicate_messages
+      message = { key: "1234", type: "dummy_event" }
+
+      DummyConsumer.new.process(message)
+      DummyConsumer.new.process(message)
+
+      assert_equal 1, EventHandlers::DummyEvent.times_called
     end
   end
 end
