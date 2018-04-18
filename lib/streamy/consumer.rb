b9144e009766b857f2f4fe5e3d9f1fb4e910505a
Prem Sichanugrist <s@sikac.hu>
2018-04-19 03:41:32 +0700

    Add `pause_consumer!` helper
    This helper can be placed on top of the file to pause the consumer from
    consuming the event on the queue. The queue and the routing of the
    message will still be created as if this is a live consumer.
    
    Usage:
    
        class SomeConsumer
          include Streamy::Consumer
          pause_consumer!
        end

--- lib/streamy/consumer.rb
+++ lib/streamy/consumer.rb
@@ -4,6 +4,19 @@ module Streamy
   module Consumer
     def self.included(base)
       base.include Hutch::Consumer
+      base.extend ClassMethods
+    end
+
+    module ClassMethods
+      def pause_consumer!
+        Hutch.consumers.delete self
+
+        Hutch::Config.setup_procs << Proc.new do
+          Hutch.logger.info("setting up paused queue: #{get_queue_name}")
+          queue = Hutch.broker.queue(get_queue_name, get_arguments)
+          Hutch.broker.bind_queue(queue, routing_keys)
+        end
+      end
     end
 
     def process(message)
