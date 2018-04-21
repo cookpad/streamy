ba5ddef74e6f942b02287d24b4897e051148c697
Jens Balvig <jens@balvig.com>
2018-04-21 09:57:31 +0900

    routing key prefix is a RabbitMQ-specific feature

--- test/rabbit_message_bus_test.rb
+++ test/rabbit_message_bus_test.rb
@@ -10,5 +10,14 @@ module Streamy
 
       bus.deliver(key: "key", topic: "topic", type: "type", body: "body", event_time: "2018")
     end
+
+    def test_deliver_with_custom_routing_prefix
+      bus = MessageBuses::RabbitMessageBus.new(uri: "valid_uri", routing_key_prefix: "replay.global")
+
+      Hutch.expects(:connect)
+      Hutch.expects(:publish).with("replay.global.topic.type", key: "key", topic: "topic", type: "type", body: "body", event_time: "2018")
+
+      bus.deliver(key: "key", topic: "topic", type: "type", body: "body", event_time: "2018")
+    end
   end
 end
