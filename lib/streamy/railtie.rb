7a4204193aa8deefca678c833351a75f77127dfb
Jens Balvig <jens@balvig.com>
2018-11-09 08:00:27 +0900

    update gem to reflect latest conventions/infrastructure (#59)

--- lib/streamy/railtie.rb
+++
@@ -1,7 +0,0 @@
-module Streamy
-  class Railtie < Rails::Railtie
-    rake_tasks do
-      load "streamy/railties/worker.rake"
-    end
-  end
-end
--- test/consumer_test.rb
+++ test/consumer_test.rb
@@ -5,20 +5,18 @@ class DummyConsumer
   include Streamy::Consumer
 end
 
-module EventHandlers
-  class DummyEvent < Streamy::EventHandler
-    cattr_accessor :times_called
+class DummyEventHandler < Streamy::EventHandler
+  cattr_accessor :times_called
 
-    def process
-      self.times_called += 1
-    end
+  def process
+    self.times_called += 1
   end
 end
 
 module Streamy
   class ConsumerTest < Minitest::Test
     def setup
-      EventHandlers::DummyEvent.times_called = 0
+      DummyEventHandler.times_called = 0
     end
 
     def test_processing_empty_message
@@ -29,24 +27,14 @@ module Streamy
       end
     end
 
-    def test_ignoring_duplicate_messages_if_cache_is_on
-      Streamy.cache = ActiveSupport::Cache::MemoryStore.new
-      message = { key: "1234", type: "dummy_event" }
-
-      DummyConsumer.new.process(message)
-      DummyConsumer.new.process(message)
-
-      assert_equal 1, EventHandlers::DummyEvent.times_called
-    end
-
-    def test_allowing_duplicate_messages_if_cache_is_off
+    def test_processing_message_twices
       Streamy.cache = nil
       message = { key: "1234", type: "dummy_event" }
 
       DummyConsumer.new.process(message)
       DummyConsumer.new.process(message)
 
-      assert_equal 2, EventHandlers::DummyEvent.times_called
+      assert_equal 2, DummyEventHandler.times_called
     end
   end
 end
