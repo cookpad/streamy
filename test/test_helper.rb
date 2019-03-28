c67b04c6ee17de638f34d173829751a1d9fbf3ee
Danny Dawson <danny-dawson@cookpad.com>
2019-03-28 16:06:36 +0000

    Remove old enviroment variables

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,3 @@ require "streamy"
 require "minitest/autorun"
 require "minitest/focus"
 require "mocha/minitest"
-
-ENV["SCHEMA_REGISTRY_URL"] = "http://registry.example.com"
-ENV["SCHEMAS_PATH"] = "test/support/schemas"
