c1332aa647a20c60f3d3c0c60f84e20418f4a45d
Danny Dawson <danny-dawson@cookpad.com>
2019-04-02 16:39:22 +0100

    Updated rspec helper for streamy

--- lib/streamy/helpers/rspec_helper.rb
+++ lib/streamy/helpers/rspec_helper.rb
@@ -3,31 +3,16 @@ require "streamy/helpers/have_hash_matcher"
 module Streamy
   module Helpers
     module RspecHelper
-      def expect_event(topic: kind_of(String), key: kind_of(String), body: nil, type: nil, event_time: nil)
+      def expect_event(topic: kind_of(String), key: kind_of(String), body: nil, type:, event_time: nil)
         expect(Streamy.message_bus.deliveries).to have_hash(
           topic: topic,
           key: key
         )
-        assert_payload_match(body, type, event_time)
-      end
 
-      def assert_payload_match(body, type, event_time)
-        payload_hash = JSON.parse(Streamy.message_bus.deliveries.first[:payload])
-        assert_body_match(body, payload_hash) if body
-        assert_type_match(type, payload_hash) if type
-        assert_event_time_match(event_time, payload_hash) if event_time
-      end
-
-      def assert_body_match(body, payload_hash)
-        expect(body.stringify_keys).to eq(payload_hash["body"])
-      end
-
-      def assert_type_match(type, payload_hash)
-        expect(type).to eq(payload_hash["type"])
-      end
-
-      def assert_event_time_match(event_time, payload_hash)
-        expect(event_time).to eq(payload_hash["event_time"])
+        payload_hash = JSON.parse(Streamy.message_bus.deliveries.last[:payload]).deep_symbolize_keys
+        expect(body).to eq(payload_hash[:body]) if body
+        expect(type).to eq(payload_hash[:type])
+        expect(event_time).to eq(payload_hash[:event_time]) if event_time
       end
       alias expect_published_event expect_event
 
