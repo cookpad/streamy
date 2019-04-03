f07c00bff1552cf7cc94aa43b9bdf95b185f9ab8
Danny Dawson <danny-dawson@cookpad.com>
2019-04-03 16:08:40 +0100

    Updated rspec helper

--- lib/streamy/helpers/rspec_helper.rb
+++ lib/streamy/helpers/rspec_helper.rb
@@ -3,17 +3,27 @@ require "streamy/helpers/have_hash_matcher"
 module Streamy
   module Helpers
     module RspecHelper
-      def expect_event(topic: kind_of(String), key: kind_of(String), body: nil, type:, event_time: nil)
-        expect(Streamy.message_bus.deliveries).to have_hash(
-          topic: topic,
-          key: key
-        )
+      def expect_event(topic: kind_of(String), key: kind_of(String), body: kind_of(Hash), type:, event_time: nil)
+        deliveries = normalize_deliveries(Streamy.message_bus.deliveries)
 
-        payload_hash = JSON.parse(Streamy.message_bus.deliveries.last[:payload]).deep_symbolize_keys
-        expect(payload_hash[:body]).to match(body) if body
-        expect(type).to eq(payload_hash[:type])
-        expect(event_time).to eq(payload_hash[:event_time]) if event_time
+        expect(deliveries).to have_hash(
+          topic: topic,
+          key: key,
+          payload: {
+            body: body,
+            type: type,
+            event_time: event_time || kind_of(Time)
+          }
+        )
       end
+
+      def normalize_deliveries(message_bus_deliveries)
+        message_bus_deliveries.each do |message|
+          message[:payload] = JSON.parse(message[:payload]).deep_symbolize_keys
+          message
+        end
+      end
+
       alias expect_published_event expect_event
 
       Streamy.message_bus = MessageBuses::TestMessageBus.new
