2e8ce08f51e22739afd144e2cc7be45215958cf8
Danny Dawson <danny-dawson@cookpad.com>
2019-04-03 16:36:17 +0100

    Updated rspec helper

--- lib/streamy/helpers/rspec_helper.rb
+++ lib/streamy/helpers/rspec_helper.rb
@@ -3,8 +3,8 @@ require "streamy/helpers/have_hash_matcher"
 module Streamy
   module Helpers
     module RspecHelper
-      def expect_event(topic: kind_of(String), key: kind_of(String), body: kind_of(Hash), type:, event_time: nil)
-        deliveries = normalize_deliveries(Streamy.message_bus.deliveries)
+      def expect_event(topic: kind_of(String), key: kind_of(String), body: kind_of(Hash), type:, event_time: kind_of(String))
+        deliveries = hashify_messages(Streamy.message_bus.deliveries)
 
         expect(deliveries).to have_hash(
           topic: topic,
@@ -12,13 +12,13 @@ module Streamy
           payload: {
             body: body,
             type: type,
-            event_time: event_time || kind_of(Time)
+            event_time: event_time
           }
         )
       end
 
-      def normalize_deliveries(message_bus_deliveries)
-        message_bus_deliveries.each do |message|
+      def hashify_messages(message_bus_deliveries)
+        message_bus_deliveries.map do |message|
           message[:payload] = JSON.parse(message[:payload]).deep_symbolize_keys
           message
         end
