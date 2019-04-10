8dc5c595d7a14015763f4d7286001c1cc8944a2b
Christian Bruckmayer <christian@bruckmayer.net>
2019-04-10 10:24:04 +0100

    Add priority to rspec test helper

--- lib/streamy/helpers/rspec_helper.rb
+++ lib/streamy/helpers/rspec_helper.rb
@@ -3,7 +3,7 @@ require "streamy/helpers/have_hash_matcher"
 module Streamy
   module Helpers
     module RspecHelper
-      def expect_event(topic: kind_of(String), key: kind_of(String), body: kind_of(Hash), type:, event_time: kind_of(String))
+      def expect_event(topic: kind_of(String), key: kind_of(String), body: kind_of(Hash), type:, event_time: kind_of(String), priority: kind_of(String))
         deliveries = hashify_messages(Streamy.message_bus.deliveries)
 
         expect(deliveries).to have_hash(
@@ -13,7 +13,8 @@ module Streamy
             body: body,
             type: type,
             event_time: event_time
-          }
+          },
+          priority: priority
         )
       end
 
