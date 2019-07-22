6ecf7b92100d89fe597f61a04de3fb9b38146a37
Danny Dawson <danny-dawson@cookpad.com>
2019-07-22 13:58:33 +0100

    Remove config from streamy and updated readme

--- lib/streamy/configuration.rb
+++ lib/streamy/configuration.rb
@@ -1,11 +1,10 @@
 module Streamy
   class Configuration
-    attr_accessor :avro_schema_registry_url, :avro_schemas_path, :producer_batched_message_limit
+    attr_accessor :avro_schema_registry_url, :avro_schemas_path
 
     def initialize
       @avro_schema_registry_url = nil
       @avro_schemas_path = nil
-      @producer_batched_message_limit = 1000
     end
   end
 end
