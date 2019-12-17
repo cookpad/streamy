2c9b67e8f94259247d8a27c7dadeff3d9afa13ff
Jesse Clark <jesse-clark@cookpad.com>
2019-12-17 16:24:34 +0000

    Check for type inside 'payload' hash

--- lib/streamy/helpers/rspec_helper.rb
+++ lib/streamy/helpers/rspec_helper.rb
@@ -17,8 +17,8 @@ module Streamy
         )
       end
 
-      def expect_no_event(type)
-        expect(streamy_deliveries).not_to include(a_hash_including(type: type.to_s))
+      def expect_no_event(type: kind_of(String))
+        expect(streamy_deliveries).not_to have_hash(payload: a_hash_including(type: type.to_s))
       end
 
       def expect_events
